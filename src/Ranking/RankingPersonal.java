b6e43dd5aedd02e3de08c03ea0e22b61b24281d2
mapu77 <edu.maura.puig@gmail.com>
2015-10-26 08:58:40 +0100

    Change Time for Calendar

--- src/Ranking/RankingPersonal.java
+++ src/Ranking/RankingPersonal.java
@@ -6,25 +6,26 @@ public class RankingPersonal extends Ranking {
 	private String usuari;
 	private int jocsResolts;
 	private double avgPistes;
-	private ArrayList<Date> bestTimes;
+	private ArrayList<Calendar> bestTimes;
 	private ArrayList<Dificultat> dificultats;
 	
 	private void ompleatributs() {
-		double n=0;
 		for (int i=0; i<super.Info.size(); ++i) {
 			String[] s = super.Info.get(i).split("//s");
 			if (s[0] == usuari) {
-				++n;
 				++jocsResolts;
 				avgPistes += Double.parseDouble(s[3]);
 				
 			}
 		}
+		avgPistes = avgPistes/(double)jocsResolts;
 	}
 	
 	public RankingPersonal(String usuari) {
 		this.usuari = usuari;
 		super.Info = CTRLRanking.carregar();
+		avgPistes = jocsResolts = 0;
+		bestTimes = ArrayList<Date>
 		this.ompleatributs();
 	}
 	

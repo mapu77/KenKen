c2789dcb8ab2251f0cf89d366a724ab2bfcef988
mapu77 <edu.maura.puig@gmail.com>
2015-10-30 00:04:04 +0100

    Update RankingPersonal

--- src/Ranking/RankingPersonal.java
+++ src/Ranking/RankingPersonal.java
@@ -1,5 +1,6 @@
 package Ranking;
 
+import java.io.*;
 import java.util.*;
 import Dificultat.*;
 
@@ -12,8 +13,7 @@ public class RankingPersonal extends Ranking {
 	private void ompleatributs() {
 		for (int i=0; i<Info.size(); ++i) {
 			ArrayList<String> s = Info.get(i);
-			String user;
-			user = new String(s.get(0));
+			String user = new String(s.get(0));
 			String dif = (Dificultat.esValida(s.get(2))) ? s.get(2) : null;
 			double time = Double.parseDouble(s.get(3));
 			int pistes = Integer.parseInt(s.get(4));
@@ -31,15 +31,30 @@ public class RankingPersonal extends Ranking {
 	}
 	
 	public RankingPersonal(String usuari) {
-		if (CtrlUser.getUser(usuari) != null) {
+		System.out.println("Crida al CTRLUsuari per comprovar si l'usuari"
+				+ " del qual volem realitzar la consulta, existeix");
+		//if (CTRLUser.getUser(usuari) != null) {
 			this.usuari = usuari;
-			super.Info = CTRLRanking.carregar();
-			this.avgPistes = this.jocsResolts = 0;
-			this.ompleatributs();
-		}
-		else {
+			//Info = CTRLRanking.carregar();
+			System.out.println("Crida al CTRLRanking per carregar les partides");
+			System.out.println("Les carreguem localment");
+			try {
+				BufferedReader bf = new BufferedReader(new FileReader("/home/edu/Documentos/FIB/5qt_15-16/PROP/KenKen/data/Partides.txt"));
+				String linia;
+				while ((linia = bf.readLine()) != null) {
+					ArrayList<String> s = new ArrayList<String>(Arrays.asList(linia.split("\\s")));
+					Info.add(s);
+				}
+				this.avgPistes = this.jocsResolts = 0;
+				this.ompleatributs();
+				bf.close();
+			} catch (IOException e) {
+				System.err.println("Error: " + e);
+			}
+		//}
+		/*else {
 			System.err.println("No existeix l'usuari");
-		}
+		}*/
 	}
 	
 	public String getUsuari() {
@@ -54,7 +69,12 @@ public class RankingPersonal extends Ranking {
 		return avgPistes;
 	}
 
-	public Map<Dificultat,Double> getBestTime() {
+	public Map<String,Double> getBestTime() {
 		return bestTime;
+	}
+	
+	public Double getBestTime(String dificultat) {
+		return bestTime.get(dificultat);
+		
 	}
 }

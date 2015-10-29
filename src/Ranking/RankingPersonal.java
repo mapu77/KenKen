4303ca20c9718cb7d25625c81faad413ccd72d0d
mapu77 <edu.maura.puig@gmail.com>
2015-10-29 20:38:02 +0100

    Update RankingPersonal

--- src/Ranking/RankingPersonal.java
+++ src/Ranking/RankingPersonal.java
@@ -7,33 +7,39 @@ public class RankingPersonal extends Ranking {
 	private String usuari;
 	private int jocsResolts;
 	private double avgPistes;
-	private Map<Dificultat,Double> bestTime;
+	private Map<String,Double> bestTime;
 	
 	private void ompleatributs() {
 		for (int i=0; i<super.Info.size(); ++i) {
 			String[] s = super.Info.get(i).split("//s");
 			String user;
 			user = new String(s[0]);
-			int idjoc = Integer.parseInt(s[1]);
-			String dif = s[2];
+			String dif = (Dificultat.esValida(s[2])) ? s[2] : null;
 			double time = Double.parseDouble(s[3]);
 			int pistes = Integer.parseInt(s[4]);
 			if (usuari.equals(user)) {
 				++jocsResolts;
 				avgPistes += pistes;
-				if (bestTime.containsKey(dif) && 
-						bestTime.get(dif) > time) bestTime.put(dif,time);
-				else bestTime.put(dif,time);
+				if (dif != null) {
+					if (bestTime.containsKey(dif) && bestTime.get(dif) > time) 
+						bestTime.put(dif,time);						
+					else bestTime.put(dif,time);
+				}
 			}
 		}
 		avgPistes = avgPistes/(double)jocsResolts;
 	}
 	
 	public RankingPersonal(String usuari) {
-		this.usuari = usuari;
-		super.Info = CTRLRanking.carregar();
-		avgPistes = jocsResolts = 0;
-		this.ompleatributs();
+		if (CtrlUser.getUser(usuari) != null) {
+			this.usuari = usuari;
+			super.Info = CTRLRanking.carregar();
+			this.avgPistes = this.jocsResolts = 0;
+			this.ompleatributs();
+		}
+		else {
+			System.err.println("No existeix l'usuari");
+		}
 	}
 	
 	public String getUsuari() {

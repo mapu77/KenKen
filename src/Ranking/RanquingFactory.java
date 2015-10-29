75d76b52b1e4ec79fb9f38b11b582c846874f114
OriolCapo <oriolcapo@gmail.com>
2015-10-29 11:04:39 +0100

    RankingFactory

---
+++ src/Ranking/RanquingFactory.java
@@ -0,0 +1,14 @@
+public class RanquingFactory {
+	
+	public RankingGeneral generarRankGen() {
+		RankingGeneral();
+	}
+	
+	public RankingTipus generarRankTipus (String Dificultat) {
+		int a;
+	}
+	
+	public RankigPersonal generarRankPersonal (String username) {
+		
+	}
+}

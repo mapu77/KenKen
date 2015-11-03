7e8d7aad90fbdaf1156f5c10f3a35dc3ec7b6d01
mapu77 <edu.maura.puig@gmail.com>
2015-11-03 10:16:06 +0100

    Default version

--- src/Ranking/CTRLRanking.java
+++ src/Ranking/CTRLRanking.java
@@ -5,21 +5,8 @@ import java.util.*;
 
 public class CTRLRanking {
 	private ArrayList<ArrayList<String>> Info;
-	
-	private void simularlectura() throws IOException {
-		FileReader f = new FileReader("/data/Partides.txt");
-		BufferedReader bf = new BufferedReader(f);
-		String s;
-		int i=0;
-		while ((s=bf.readLine()) != null) {
-			Info.get(i).add(s);
-			++i;
-		}
-		bf.close();
-	}
-	
-	public static ArrayList<String> carregar() {
 		
-		return null;	
+	public static void carregar() {
+		
 	}
 }

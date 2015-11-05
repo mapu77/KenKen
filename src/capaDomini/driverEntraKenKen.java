c3ff9faaa7e8fab85da90b1eda9203e1e1388900
mapu77 <edu.maura.puig@gmail.com>
2015-11-05 20:24:35 +0100

    trying to fix entry of values

--- src/capaDomini/driverEntraKenKen.java
+++ src/capaDomini/driverEntraKenKen.java
@@ -1,9 +1,7 @@
 package capaDomini;
 
 import java.util.*;
-
-
-//import capaDomini.*;
+import Algoritmes.KenKenSolver;
 
 public class driverEntraKenKen {
 	
@@ -11,27 +9,37 @@ public class driverEntraKenKen {
 		Scanner sn = new Scanner(System.in);
 		System.out.println("Mida del KenKen");
 		int n = Integer.parseInt(sn.nextLine());
+		System.out.println("He llegit " + n);
 		TaulerKenKen T = new TaulerKenKen(n);
 		System.out.println("Nombre regions");
 		int nr = Integer.parseInt(sn.nextLine());
+		System.out.println("He llegit " + nr);
 		for (int i=1; i<=nr; ++i) {
 			RegioKenKen r = new RegioKenKen(nr);
 			r.setId(i);
 			System.out.println("Operacio de la regio " + i);
-			r.setOperation(sn.nextLine());
+			String op = sn.nextLine();
+			r.setOperation(op);
+			System.out.println("He llegit " + op);
 			System.out.println("Resultat de la regio" + i);
-			r.setResult(Integer.parseInt(sn.nextLine()));
+			int res = Integer.parseInt(sn.nextLine());
+			r.setResult(res);
+			System.out.println("He llegit " + res);
 			System.out.println("Nombre cel.les de la regio " + i);
 			int nc = sn.nextInt();
+			System.out.println("He llegit " + nc);
 			for (int j=1; j<=nc; ++j) {
 				System.out.println("Cordenades cel.la " + j + " de la regio " + i);
-				int x = (sn.nextInt());
+				int x = (sn.nextInt()); //<------ peta aqui perque llegiex coses que no son
 				int y = (sn.nextInt());
+				System.out.println("He llegit " + x + " " +y);
 				Cella c = new Cella(x,y);
 				r.Relaciona(j,c);	
 			}
 			T.afegeixRegio(r);
 		}
+		
+		/*
 		int x;
 		System.out.println("Entra els valors");
 		for (int i = 0; i < n; ++i) {
@@ -41,6 +49,10 @@ public class driverEntraKenKen {
 				T.setNumero(i,j,x);
 			}
 		}
+		*/
+		
+		KenKenSolver ks = new KenKenSolver();
+		ks.backtrackingSolver(T);
 		
 		for (int i=0; i<n; ++i) {
 			for (int j=0; j<n; ++j) {

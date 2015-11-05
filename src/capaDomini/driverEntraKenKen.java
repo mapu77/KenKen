e307d2b3d10a50a0b638a813a6bd60fab790ffed
OriolCapo <oriolcapo@gmail.com>
2015-11-05 20:56:14 +0100

    aa
    aa

--- src/capaDomini/driverEntraKenKen.java
+++ src/capaDomini/driverEntraKenKen.java
@@ -1,65 +1,46 @@
 package capaDomini;
 
 import java.util.*;
-import Algoritmes.KenKenSolver;
+
+
+//import capaDomini.*;
 
 public class driverEntraKenKen {
 	
 	public static void main(String[] args) {
 		Scanner sn = new Scanner(System.in);
 		System.out.println("Mida del KenKen");
-		int n = Integer.parseInt(sn.nextLine());
-		System.out.println("He llegit " + n);
-		TaulerKenKen T = new TaulerKenKen(n);
+		int n = sn.nextInt();
+		TaulerKenKen T = new TaulerKenKen(n,n);
 		System.out.println("Nombre regions");
-		int nr = Integer.parseInt(sn.nextLine());
-		System.out.println("He llegit " + nr);
+		int nr = sn.nextInt();
 		for (int i=1; i<=nr; ++i) {
 			RegioKenKen r = new RegioKenKen(nr);
 			r.setId(i);
 			System.out.println("Operacio de la regio " + i);
-			String op = sn.nextLine();
+			String op = sn.next();
 			r.setOperation(op);
-			System.out.println("He llegit " + op);
-			System.out.println("Resultat de la regio" + i);
-			int res = Integer.parseInt(sn.nextLine());
+			System.out.println("Resultat de la regio " + i);
+			int res = sn.nextInt();
 			r.setResult(res);
-			System.out.println("He llegit " + res);
-			System.out.println("Nombre cel.les de la regio " + i);
+			System.out.println("Cel·les de la regio " + i);
 			int nc = sn.nextInt();
-			System.out.println("He llegit " + nc);
-			for (int j=1; j<=nc; ++j) {
-				System.out.println("Cordenades cel.la " + j + " de la regio " + i);
-				int x = (sn.nextInt()); //<------ peta aqui perque llegiex coses que no son
-				int y = (sn.nextInt());
-				System.out.println("He llegit " + x + " " +y);
-				Cella c = new Cella(x,y);
+			for (int j=0; j<nc; ++j) {
+				System.out.println("Cordenades cel·la " + j + " de la regio " + i);
+				Cella c = new Cella(sn.nextInt(),sn.nextInt());
 				r.Relaciona(j,c);	
 			}
 			T.afegeixRegio(r);
 		}
-		
-		/*
+		System.out.println("Introdueix la teva solucio");
 		int x;
-		System.out.println("Entra els valors");
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < n; ++j) {
-				System.out.println("Valor per la cel.la (" + (i+1) +","+ (j+1) +")");
 				x = sn.nextInt();
 				T.setNumero(i,j,x);
 			}
 		}
-		*/
-		
-		KenKenSolver ks = new KenKenSolver();
-		ks.backtrackingSolver(T);
-		
-		for (int i=0; i<n; ++i) {
-			for (int j=0; j<n; ++j) {
-				System.out.print(T.getNumero(i,j)+ " ");
-			}
-			System.out.println();
-		}
+		T.PrintaKenKen();
 		sn.close();
 	}
 }

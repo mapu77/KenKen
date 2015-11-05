21b7e21b0320461f378a23eecb3d96aec4eef8db
mapu77 <edu.maura.puig@gmail.com>
2015-11-05 19:53:46 +0100

    Add operacio regio i resultat

--- src/capaDomini/driverEntraKenKen.java
+++ src/capaDomini/driverEntraKenKen.java
@@ -17,10 +17,14 @@ public class driverEntraKenKen {
 		for (int i=1; i<=nr; ++i) {
 			RegioKenKen r = new RegioKenKen(nr);
 			r.setId(i);
-			System.out.println("Cel�les de la regio " + i);
+			System.out.println("Operacio de la regio " + i);
+			r.setOperation(sn.nextLine());
+			System.out.println("Resultat de la regio" + i);
+			r.setResult(Integer.parseInt(sn.nextLine()));
+			System.out.println("Nombre cel.les de la regio " + i);
 			int nc = sn.nextInt();
-			for (int j=0; j<nc; ++j) {
-				System.out.println("Cordenades cel�la " + j + " de la regio " + i);
+			for (int j=1; j<=nc; ++j) {
+				System.out.println("Cordenades cel.la " + j + " de la regio " + i);
 				int x = (sn.nextInt());
 				int y = (sn.nextInt());
 				Cella c = new Cella(x,y);
@@ -29,8 +33,10 @@ public class driverEntraKenKen {
 			T.afegeixRegio(r);
 		}
 		int x;
+		System.out.println("Entra els valors");
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < n; ++j) {
+				System.out.println("Valor per la cel.la (" + (i+1) +","+ (j+1) +")");
 				x = sn.nextInt();
 				T.setNumero(i,j,x);
 			}

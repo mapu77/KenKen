5d5a86f5784b41edfaa5d5c87b84e0506cf8263a
JanTe13 <jan_teru@hotmail.com>
2015-11-06 13:21:07 +0100

    Canvis RGeneral

--- src/Ranking/RankingGeneral.java
+++ src/Ranking/RankingGeneral.java
@@ -18,34 +18,42 @@ public class RankingGeneral extends Ranking {
 	
 	public void inicialitza() {
 		//nombre de jocs
-		CTRLRanking.carregar(this, "Jocs");
-		ArrayList<Integer> cont = new ArrayList<Integer>(Info.size());
+		stubCTRLRankingJocs.carregar(this, "Jocs");
 		this.nJocs = Info.size();
+		
 		//nombre d'usuaris
-		CTRLRanking.carregar(this, "Usuaris");
+		stubCTRLRankingUsuaris.carregar(this, "Usuaris");
 		this.nUsuaris = Info.size();
+		
 		//nombre de partides
-		CTRLRanking.carregar(this,  "Partides");
+		stubCTRLRankingPartides.carregar(this, "Partides");
 		this.nPartides = Info.size();
+		
 		//joc més popular
-		for (int i = 0; i < cont.size(); ++i) cont.add(i, 0);
-		
-		CTRLRanking.carregar(this, "Partides");
-		for (int i = 0; i < Info.size(); ++i) {
-			ArrayList<String> s = Info.get(i);
-			String id = s.get(1);
-			int posicio = Integer.parseInt(id)-1;
-			int valor = cont.get(posicio);
-			cont.add(posicio, ++valor);
+		Map<String,Integer> cont = new HashMap<>();
+		stubCTRLRankingPartides.carregar(this, "Partides");
+		for (int i = 0; i < Info.size(); ++i) {	//presència d'un joc a Partides
+			ArrayList<String> s = Info.get(i);	//agafem línia per línia la informació Info
+			String id = s.get(1);	//prenem el valor identificador que ens interessa
+			if (!(cont.containsKey(id))) {	//no havia aparegut
+				cont.put(id,1);	//afegim <Key = id, value = 1> a la primera posició lliure
+			}
+			else {	//ja havia aparegut
+				int value = cont.get(id);	//consultem el valor del Key = id
+				cont.put(id,++value);	//afegim <value += 1> a la posició de Key = id
+			}
 		}
-		
-		int max = cont.get(0);
-    	this.popular = Integer.toString(1);
-    	for (int i = 1; i < cont.size(); ++i) {
-    		if (max < cont.get(i)) {
-    			max = cont.get(i);
-    			this.popular = Integer.toString(i+1);
+		Map.Entry<String,Integer> entry;
+		int max = -1;	//assignem valor a max
+		int visitats = 0;	//posicions visitades
+    	while (visitats <= cont.size()) {
+    		entry = cont.entrySet().iterator().next();	//prenem els valors de la primera posició
+    		if (max < entry.getValue()) {
+    			max = entry.getValue();
+    			this.popular = entry.getKey();
     		}
+    		++visitats;
+    		cont.remove(entry.getKey());
     	}
 	}
 	
@@ -65,68 +73,3 @@ public class RankingGeneral extends Ranking {
     	return this.popular;
 	}
 }
-	
-/*------------------------------------------------------------------------
-	
-	public int getnJocs() {
-		try {
-			this.nJocs = 0;
-			// Obrim l'arxiu
-            FileInputStream fstream = new FileInputStream("C:\\Users\\Jan\\Programes\\eclipse\\eclipse-epsilon-1.2-win32\\workspace\\KenKen_PROP\\Jocs.txt");
-            // Creem l'objecte d'entrada
-            DataInputStream entrada = new DataInputStream(fstream);
-            // Creem el buffer de lectura
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-            // Llegim l'arxiu línia per línia
-            while (buffer.readLine() != null)   {
-                // Incrementem nJocs
-                this.nJocs++;
-            }
-            // Tanquem l'arxiu
-            entrada.close();
-        }
-		catch (Exception e){ //Catch d'excepcions
-            System.err.println("Hi ha hagut un error: " + e.getMessage());
-        }
-
-	public String getPopular(int N) {
-		try {
-			// Obrim l'arxiu
-            FileInputStream fstream = new FileInputStream("C:\\Users\\Jan\\Programes\\eclipse\\eclipse-epsilon-1.2-win32\\workspace\\KenKen_PROP\\Partides.txt");
-            // Creem l'objecte d'entrada
-            DataInputStream entrada = new DataInputStream(fstream);
-            // Creem el buffer de lectura
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-            //Creem vector de popularitat
-            ArrayList<Integer> cont = new ArrayList<Integer>(N);
-            //Inicialitzem cont a 0
-            for (int i = 0; i < N; ++i) {
-        		cont.add(0);
-        	}
-            // Llegim l'arxiu línia per línia
-            String linea;
-        	while ((linea = buffer.readLine()) != null) {
-        		//Dividim l'string a cada espai
-        		String[] s = linea.split(" ");
-        		int posicio = Integer.parseInt(s[1]) - 1;
-        		int valor = cont.get(posicio);
-        		cont.add(posicio, ++valor);
-        	}
-        	int max = cont.get(0);
-        	this.popular = Integer.toString(1);
-        	for (int i = 1; i < N; ++i) {
-        		if (max < cont.get(i)) {
-        			max = cont.get(i);
-        			this.popular = Integer.toString(i+1);
-        		}
-        	}
-            // Tanquem l'arxiu
-            entrada.close();
-        }
-		catch (Exception e){ //Catch d'excepcions
-            System.err.println("Hi ha hagut un error: " + e.getMessage());
-        }
-		return this.popular;
-	}
-	
-	*/

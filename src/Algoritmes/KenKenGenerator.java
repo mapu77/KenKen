a48428389be3c9546076da75a828d0490e6b49f5
mapu77 <edu.maura.puig@gmail.com>
2015-11-07 14:26:25 +0100

    Improving generateNumbers

--- src/Algoritmes/KenKenGenerator.java
+++ src/Algoritmes/KenKenGenerator.java
@@ -8,18 +8,17 @@ public class KenKenGenerator {
 	private Vector<Boolean> used; //llistat del 0 a n+1 de valors usats
 	private static TaulerKenKen K;
 	private int n;
-	Random random;
+	private static boolean fi;
 	private int X[] = {1,0,-1,0};
 	private int Y[] = {0,1,0,-1};
 	private static double probStop;
 	
 	public KenKenGenerator() {
-		Random random = new Random();
 	}
 	
 	/* Retorna si x és un nombre valid per la fila r */
 	private static boolean checkFila(int fila, int x) {
-		for (int col=0; col<K.getNCols(); ++col) {
+		for (int col=0; col<K.getAncho(); ++col) {
 			if (K.getCella(fila, col).getNumero() == x) return false;
 		}
 		return true;
@@ -27,49 +26,51 @@ public class KenKenGenerator {
 	
 	/* Retorna si x és un nombre vàlid per la columna c */
 	private static boolean checkCol(int col, int x) {
-		for (int fila = 0; fila < K.getNFiles(); ++fila) {
+		for (int fila = 0; fila < K.getAlto(); ++fila) {
 			if (K.getCella(fila, col).getNumero() == x) return false;
 		}
 		return true;
 	}
 	
 	private void backtrackingGenerateNumbers(int i, int j, int max) {
-		if ((i+1)*(j+1) < max) {
-			int rand = random.nextInt(n-1)+1; //genera nombre aleatori entre 1 i n
-			for (int k=1; k<=n; ++k) {
-				for (int ii=1; ii<=n; ++i) used.set(i, false);
-				while (!used.get(rand)) rand = random.nextInt(n-1)+1;
-				used.set(rand, true);
-				K.setNumero(i, j, rand);
+		if ((i+1)*(j+1) == max) {
+			fi = true;
+		}
+		else {
+			int rand = new Random().nextInt(n)+1; //genera nombre aleatori entre 1 i n
+			for (int k=1; k<=n && !fi; ++k) {
+				for (int ii=1; ii<used.size(); ++ii) used.set(ii,false);
+				while (used.get(rand)) rand = new Random().nextInt(n)+1;
+				used.set(rand,true);
 				if (checkFila(i,rand) && checkCol(j,rand)) {
-					if (j==9) {
-						j=0;
-						++i;
+					K.setNumero(i,j,rand);
+					if (j==n) {
+						backtrackingGenerateNumbers(i+1,0,max);
 					}
 					else {
-						++j;
+						backtrackingGenerateNumbers(i,j+1,max);
 					}
-					backtrackingGenerateNumbers(i,j,max);
 				}				
 			}
+			if (!fi) K.borra(i, j);
 		}
 	}
 	
 	private void generateRegions() {
 		int reg_count = 1;
-		for (int i=0; i<K.getancho(); ++i) {
-			for (int j=0; j<K.getalto(); ++j) {
+		for (int i=0; i<K.getAncho(); ++i) {
+			for (int j=0; j<K.getAlto(); ++j) {
 				if (K.nRegio(i, j) == 0) { // no te regio
 					Stack<Cella> s = new Stack<Cella>();
 					Vector<Cella> vc = new Vector<Cella>();
 					probStop = 0.3;
 					vc.add(new Cella(i,j));
 					s.push(new Cella(i,j));
-					while (random.nextDouble() > probStop) { //No parem
-						int rand = random.nextInt(3);
+					while (new Random().nextDouble() > probStop) { //No parem
+						int rand = new Random().nextInt(3);
 						int ii = s.peek().getX()+Y[rand];
 						int jj = s.peek().getY()+X[rand];
-						if (ii >= 0 && ii < K.getancho() && jj >= 0 && jj < K.getalto()){
+						if (ii >= 0 && ii < K.getAncho() && jj >= 0 && jj < K.getAlto()){
 							if (!vc.contains(new Cella(ii,jj))) {
 								vc.add(new Cella(ii,jj));
 								s.push(new Cella(ii,jj));
@@ -92,12 +93,13 @@ public class KenKenGenerator {
 	
 	public TaulerKenKen generateRandomly(int size) {
 		n = size;
+		fi = false;
 		K = new TaulerKenKen(n);
-		used = new Vector<Boolean> (n+1);
-		for (int i=0; i<=used.size(); ++i) used.set(i, false);
+		used = new Vector<Boolean> ();
+		for (int i=0; i<n+1; ++i) used.add(i,false);
 		backtrackingGenerateNumbers(0,0,K.getNumCeldas());
-		generateRegions();
-		generateRegionSolution();
+		//generateRegions();
+		//generateRegionSolution();
 		return K;
 	}
 	

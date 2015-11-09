d18c8b864ba2016f9194187c7c6c321547e02f4c
mapu77 <edu.maura.puig@gmail.com>
2015-11-10 00:29:18 +0100

    Consistent amb els nous User, CtrlUser i CtrlPersistencia

--- src/Ranking/RankingGeneral.java
+++ src/Ranking/RankingGeneral.java
@@ -18,36 +18,36 @@ public class RankingGeneral extends Ranking {
 	
 	public void inicialitza() {
 		//nombre de jocs
-		stubCTRLRankingJocs.carregar(this, "Jocs");
+		CTRLRanking.carregar(this, "Jocs");
 		this.nJocs = Info.size();
 		
 		//nombre d'usuaris
-		stubCTRLRankingUsuaris.carregar(this, "Usuaris");
+		CTRLRanking.carregar(this, "Usuaris");
 		this.nUsuaris = Info.size();
 		
 		//nombre de partides
-		stubCTRLRankingPartides.carregar(this, "Partides");
+		CTRLRanking.carregar(this, "Partides");
 		this.nPartides = Info.size();
 		
-		//joc més popular
+		//joc mï¿½s popular
 		Map<String,Integer> cont = new HashMap<>();
-		stubCTRLRankingPartides.carregar(this, "Partides");
-		for (int i = 0; i < Info.size(); ++i) {	//presència d'un joc a Partides
-			ArrayList<String> s = Info.get(i);	//agafem línia per línia la informació Info
+		CTRLRanking.carregar(this, "Partides");
+		for (int i = 0; i < Info.size(); ++i) {	//presï¿½ncia d'un joc a Partides
+			ArrayList<String> s = Info.get(i);	//agafem lï¿½nia per lï¿½nia la informaciï¿½ Info
 			String id = s.get(1);	//prenem el valor identificador que ens interessa
 			if (!(cont.containsKey(id))) {	//no havia aparegut
-				cont.put(id,1);	//afegim <Key = id, value = 1> a la primera posició lliure
+				cont.put(id,1);	//afegim <Key = id, value = 1> a la primera posiciï¿½ lliure
 			}
 			else {	//ja havia aparegut
 				int value = cont.get(id);	//consultem el valor del Key = id
-				cont.put(id,++value);	//afegim <value += 1> a la posició de Key = id
+				cont.put(id,++value);	//afegim <value += 1> a la posiciï¿½ de Key = id
 			}
 		}
 		Map.Entry<String,Integer> entry;
 		int max = -1;	//assignem valor a max
 		int visitats = 0;	//posicions visitades
     	while (visitats <= cont.size()) {
-    		entry = cont.entrySet().iterator().next();	//prenem els valors de la primera posició
+    		entry = cont.entrySet().iterator().next();	//prenem els valors de la primera posiciï¿½
     		if (max < entry.getValue()) {
     			max = entry.getValue();
     			this.popular = entry.getKey();

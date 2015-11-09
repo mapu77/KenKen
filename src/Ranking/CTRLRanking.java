64771f96b6a33abe117c322b1c5cfd43fc266d2f
mapu77 <edu.maura.puig@gmail.com>
2015-11-09 23:05:45 +0100

    Adding try & catch

--- src/Ranking/CTRLRanking.java
+++ src/Ranking/CTRLRanking.java
@@ -1,12 +1,16 @@
 package Ranking;
 
 import java.io.*;
-import java.util.*;
 import capaPersistencia.*;
 
 public class CTRLRanking {
 		
-	public static void carregar(Ranking r, String arxiu) throws IOException {
-		r.Info = CtrlPersistencia.loadTable("./data/" + arxiu + ".txt");
+	public static void carregar(Ranking r, String arxiu) {
+		CtrlPersistencia.setSeparator(" ");
+		try {
+			r.Info = CtrlPersistencia.loadTable("./data/" + arxiu + ".txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }

c1b7abe831a9f099506c8e2a047e549662f0f37d
mapu77 <edu.maura.puig@gmail.com>
2015-11-10 00:21:10 +0100

    Consistent amb els nous User, CtrlUser i CtrlPersistencia

--- src/Ranking/driverRP.java
+++ src/Ranking/driverRP.java
@@ -2,21 +2,25 @@ package Ranking;
 
 import java.util.*;
 import Dificultat.*;
+import capaDomini.CtrlUser;
+import capaPersistencia.CtrlPersistencia;
 
 public class driverRP {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner scan = new Scanner (System.in);
 		System.out.println("Ranking Personal");
 		System.out.println("----------------");
 		System.out.println("Opcions:");
 		System.out.println("1. Consultar rànquing");
 		System.out.println("0. Exit");
-		int opt = Integer.parseInt(scan.nextLine());
+		CtrlPersistencia.setSeparator(" ");
+		int opt = scan.nextInt();
 		while (opt != 0) {
 			if (opt == 1) {
+				CtrlUser CTRLUser = new CtrlUser();
 				System.out.println("De qui vols consultar el rànquing?");
-				String u = scan.nextLine();
+				String u = scan.next();
 				RankingPersonal RP = new RankingPersonal(u);
 				System.out.println();
 				System.out.println("Què vols consultar?");
@@ -26,7 +30,7 @@ public class driverRP {
 				System.out.println("3: Millors temps");
 				System.out.println("4: Veure-ho tot");
 				System.out.println("0: Canviar usuari");
-				int opt2 = Integer.parseInt(scan.nextLine());
+				int opt2 = scan.nextInt();
 				while (opt2 != 0) {
 					if (opt2 == 1) {
 						System.out.println(u+" ha jugat "+ RP.getResolts()+" partida/es");	
@@ -59,7 +63,7 @@ public class driverRP {
 					System.out.println("3: Millors temps");
 					System.out.println("4: Veure-ho tot");
 					System.out.println("0: Canviar usuari");
-					opt2 = Integer.parseInt(scan.nextLine());
+					opt2 = scan.nextInt();
 				}
 			}
 			else {
@@ -71,7 +75,7 @@ public class driverRP {
 			System.out.println("Opcions:");
 			System.out.println("1. Consultar rànquing");
 			System.out.println("0. Exit");
-			opt = Integer.parseInt(scan.nextLine());
+			opt = scan.nextInt();
 		}
 		System.out.println("Fi del programa");
 		scan.close();

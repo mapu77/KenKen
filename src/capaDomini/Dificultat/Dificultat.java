a5d37da8466ff9007b2f062861563360d90306df
mapu77 <edu.maura.puig@gmail.com>
2015-11-11 11:43:33 +0100

    Package Correcte

---
+++ src/capaDomini/Dificultat/Dificultat.java
@@ -0,0 +1,16 @@
+package capaDomini.Dificultat;
+
+abstract public class Dificultat {
+	private static final String[] d = {"3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9"};
+	
+	public static boolean esValida(String s) {
+		for (int i=0; i<d.length; ++i) {
+			if (d[i].equals(s)) return true;
+		}
+		return false;
+	}
+	
+	public static String[] getAll() {
+		return d;
+	}
+}

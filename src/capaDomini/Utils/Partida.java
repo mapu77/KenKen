d29beb368cea89794675bae1d78dbf4b395c18cb
mapu77 <edu.maura.puig@gmail.com>
2015-11-11 13:37:06 +0100

    Adding Partida

--- src/capaDomini/Utils/Partida.java
+++ src/capaDomini/Utils/Partida.java
@@ -1,26 +1,102 @@
 package capaDomini.Utils;
 
+import capaDomini.Algoritmes.KenKenGenerator;
+import capaDomini.Dificultat.Dificultat;
+
 public class Partida {
 	private String usuari;
-	private String idJoc;
-	private String Dificultat;
+	private String idJoc; 		//Identificador del tauler
+	private String D;	//Dificultat tauler
 	private int pistes;
-	private long tInit;
+	private long initialTime;
+	private long currentTime = 0;
 	private TaulerKenKen K;
 	private CtrlPartida CP;
+	private CtrlJoc CJ;
 	
-	public Partida(String u, String id) {
-		this.tInit = System.nanoTime();
+	public Partida() {
+		
+	}
+	
+	/* Creador partda amb KenKen aleatori */
+	public Partida(String u, String d) {
 		CP = new CtrlPartida();
+		CJ = new CtrlJoc();
+		this.D = d;
+		int n;
+		n = Dificultat.toInt(d);
+		K = new KenKenGenerator().generateRandomly(n);
+	}
+	
+	public Partida(String u, String d, String id) {
+		CP = new CtrlPartida();
+		CJ = new CtrlJoc();
+		this.D = d;
 	}
 	
 	/* Guarda a la BD una partida acabada */
-	public void save() {
+	private void save() {
 		CP.save(this);
 	}
 
 	/* Guarda a la BD l'estat del KenKen */
 	public void saveKenKen() {
 	
+	}
+	
+	public void play() {
+		this.initialTime = System.nanoTime();
+	}
+
+	public void stop() {
+		this.currentTime += System.nanoTime() - initialTime;
+	}
+	
+	public void resume() {
+		this.initialTime = System.nanoTime();
+	}
+	
+	public String getUsuari() {
+		return usuari;
+	}
+
+	public void setUsuari(String usuari) {
+		this.usuari = usuari;
+	}
+
+	public String getIdJoc() {
+		return idJoc;
+	}
+
+	public void setIdJoc(String idJoc) {
+		this.idJoc = idJoc;
+	}
+
+	public String getD() {
+		return D;
+	}
+
+	public void setD(String d) {
+		D = d;
+	}
+
+	public int getPistes() {
+		return pistes;
+	}
+
+	public void setPistes(int pistes) {
+		this.pistes = pistes;
+	}
+
+	public TaulerKenKen getK() {
+		return K;
+	}
+
+	public void setK(TaulerKenKen k) {
+		K = k;
+	}
+	
+	public long getcurrentTime() {
+		return System.nanoTime() - initialTime;
 	}
 }

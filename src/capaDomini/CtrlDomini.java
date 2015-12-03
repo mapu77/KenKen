289f84548bb5a32afa5a3f64e22811ecd26df257
JanTe13 <jan_teru@hotmail.com>
2015-12-03 10:24:59 +0100

    domini

--- src/capaDomini/CtrlDomini.java
+++ src/capaDomini/CtrlDomini.java
@@ -7,7 +7,11 @@ package capaDomini;
 
 import capaDomini.Algoritmes.KenKenGenerator;
 import capaDomini.Usuari.CtrlUser;
+import capaDomini.Usuari.User;
 import capaDomini.Utils.TaulerKenKen;
+import capaDomini.Ranking.RankingGeneral;
+import capaDomini.Ranking.RankingPerTipus;
+import capaDomini.Ranking.RankingPersonal;
 import capaPersistencia.CtrlPersistencia;
 import java.util.Scanner;
 
@@ -36,5 +40,44 @@ public class CtrlDomini {
     public TaulerKenKen generaKenkenAleatori (int N) {
         KenKenGenerator KG = new KenKenGenerator(sn);
         return KG.generateRandomly(N);
+    }
+    
+    public User existeixUsuari (String u) {
+        return CtrlUser.getUsuari(u);
+    }
+    
+    public double obtenirTempsRP (String u, String diff) {
+        RankingPersonal RP = new RankingPersonal(u);
+        return RP.getBestTime(diff);
+    }
+    
+    public double obtenirPistesRP (String u) {
+        RankingPersonal RP = new RankingPersonal(u);
+        return RP.getPistes();
+    }
+    
+    public int obtenirResoltsRP (String u) {
+        RankingPersonal RP = new RankingPersonal(u);
+        return RP.getResolts();
+    }
+    
+    public int jocsRG () {
+        RankingGeneral RG = new RankingGeneral();
+        return RG.getnJocs();
+    }
+    
+    public int partidesRG () {
+        RankingGeneral RG = new RankingGeneral();
+        return RG.getnPartides();
+    }
+    
+    public int usuarisRG () {
+        RankingGeneral RG = new RankingGeneral();
+        return RG.getnUsuaris();
+    }
+    
+    public String popularRG () {
+        RankingGeneral RG = new RankingGeneral();
+        return RG.getPopular();
     }
 }
